68488e8cf366baa508fb9bad5f62c6d5791b2fd7
MINAYUAN <45114032+MINAYUAN@users.noreply.github.com>
2021-02-03 20:44:25 -0800

    Update Code-Option-Pricing.py

--- Code-Option-Pricing.py
+++ Code-Option-Pricing.py
@@ -61,3 +61,43 @@ def EURCALL2 (stock , k = 100, t = 5/365, r = 0.04):
 
 # my accuracy after reduction
 abs(BSMCALL(t=5) - EURCALL2(STOCKGBM2(t=5),t=5))
+
+# Plots
+call = np.zeros(10)
+for i in range(1,11):
+    call[i-1] = np.mean(STOCKGBM(s0 = 88, r = 0.04, sigma = 0.18, t = i,size = 1000))
+
+plt.plot(np.arange(0,11),np.hstack((88,call)))
+plt.title('Simulation of Sn, n= 1:10, S0 = 88')
+plt.show
+
+step = np.linspace(10/1000,10,1000)
+call2 = np.zeros(len(step))
+for i in range(1000):
+    call2[i] = np.mean(STOCKGBM(s0 = 88, r = 0.04, sigma = 0.18, t = step[i],size = 1000))
+
+plt.plot(np.hstack((0,step)),np.hstack((88,call2)),label='1000 steps')
+plt.plot(np.arange(0,11),np.hstack((88,call)),label='10 steps')
+plt.legend(loc = 'upper left')
+plt.title('Simulation of St, t = [1,10], S0 = 88')
+
+def STOCKGBM (s0 = 88, r=0.04, sigma = 0.2, t = 5/365, size = 1000000):
+    return(s0*np.exp(sigma*normal(0,np.sqrt(t),size=size)+ \
+                     (r-0.5*sigma**2)*t))
+    
+call3 = np.zeros(10)
+for i in range(1,11):
+    call3[i-1] = np.mean(STOCKGBM(s0 = 88, r = 0.04, sigma = 0.35, t = i,size = 1000))
+
+
+call4 = np.zeros((len(step),6))
+for n in range(6):
+    for i in range(1000):
+        call4[i,n] = np.mean(STOCKGBM(s0 = 88, r = 0.04, sigma = 0.35, t = step[i],size = 1)) # change size to have more simulation
+
+
+for i in range(6):
+    plt.plot(np.hstack((0,step)),np.hstack((88,call4[:,i])),label='1000 steps',alpha=0.9)
+plt.plot(np.arange(0,11),np.hstack((88,call3)),label='10 steps')
+plt.legend(loc = 'upper left')
+plt.title('Simulation of St, t = [1,10], S0 = 88')
